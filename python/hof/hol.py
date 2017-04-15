3f077cff3734c89c37d00e3d77a1f8e6242e6162
vastopol <classicunix@gmail.com>
2017-04-15 21:37:59 +0000

    python higher order function programming

---
+++ python/hof/hol.py
@@ -0,0 +1,20 @@
+# high order
+
+# flatten nested sublists into one list
+def flatten(list):
+    return reduce(operator.concat, list, [])
+
+def compose(f, g):
+    """Requires that f and g are functions, f:A->B and g:B->C.
+    Returns a function A->C by composing f with g.""" 
+    return lambda x: g(f(x))
+
+def chain(funcs):
+    """Requires funcs is a list of functions [A->B, B->C, ..., Y->Z]. 
+    Returns a fn A->Z that is the left-to-right composition of funcs."""
+    return reduce(compose, funcs)
+
+# returns functions that are useful as filters, it takes a filename suffix and dynamically generates a function to use with filter to test for that suffix
+def fileEndsWith(suffix):
+    return lambda file: file.getName().endsWith(suffix)
+
