e9f737bf335cfb5dbaede8fdde39af6910ab68f4
vastopol <classicunix@gmail.com>
2017-08-07 04:57:53 +0000

    start of basic py scripting

---
+++ python/basics/template.py
@@ -0,0 +1,9 @@
+# blank template
+
+import sys
+
+def main(argv):
+	pass
+
+if __name__ == '__main__':
+	main(sys.argv)
---
+++ python/quick.py
@@ -0,0 +1,11 @@
+
+import sys
+
+
+def main(argv):
+	for i in sys.path:
+		print(i)
+
+
+if __name__ == '__main__':
+	main(sys.argv)
