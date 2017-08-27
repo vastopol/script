f095f96cc45bd2ed5344a87fdc80c17419b77b09
vastopol <classicunix@gmail.com>
2017-08-27 07:56:26 +0000

    py stuffs

--- python/quick.py
+++ python/quick.py
@@ -1,11 +1,11 @@
+# python3
 
+import os
 import sys
 
 
 def main(argv):
-	for i in sys.path:
-		print(i)
-
+    print(os.getcwd())
 
 if __name__ == '__main__':
-	main(sys.argv)
+    main(sys.argv)
